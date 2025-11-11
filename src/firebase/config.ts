60999018f6ff22fcd452604be6e96acdbdb088fb
Bgmi Id <bgmihackbro@gmail.com>
2025-11-11 15:09:58 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5589810889-8713e",
+  "appId": "1:967271201856:web:3a809f573af17cb49073a5",
+  "apiKey": "AIzaSyAD-gRLJHUPP0QJ81sakOfGOtHOIkUCiLs",
+  "authDomain": "studio-5589810889-8713e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "967271201856"
+};
